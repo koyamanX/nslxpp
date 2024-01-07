60fcb09cead7b74a002f6bdbb5282d8a1b608f0b
koyamanX <ckoyama.1996@gmail.com>
2024-01-08 02:09:01 +0900

    Update

--- nslgen.cc
+++ nslgen.cc
@@ -1,4 +1,5 @@
 #include "nslgen.hh"
+#include "nslxpp.hh"
 
 NSLGen::NSLGen(std::ostream &out) : out(out)
 {
@@ -10,12 +11,118 @@ NSLGen::~NSLGen()
 
 }
 
+void NSLGen::gen_declare(const std::string &name, json &signals) {
+    out << "declare " << name;
+    gen_opening_brace();
+    for(auto &signal : signals)
+    {
+        if(signal["type"] == NSLXPP::ND_INPUT)
+        {
+            gen_input(signal);
+        }
+        else if(signal["type"] == NSLXPP::ND_OUTPUT)
+        {
+            gen_output(signal);
+        }
+        else if(signal["type"] == NSLXPP::ND_FUNC_IN)
+        {
+            gen_func_in(signal);
+        }
+        else if(signal["type"] == NSLXPP::ND_FUNC_OUT)
+        {
+            gen_func_out(signal);
+        }
+    }
+    gen_closing_brace();
+}
+
+void NSLGen::gen_input(json &signal) {
+    out << "input " << signal["name"].get<std::string>() << "[" << signal["size"] << "];" << std::endl;
+}
+
+void NSLGen::gen_output(json &signal) {
+    out << "output " << signal["name"].get<std::string>() << "[" << signal["size"] << "];" << std::endl;
+}
+
+void NSLGen::gen_func_in(json &signal) {
+    out << "func_in " << signal["name"].get<std::string>() << "(";
+    gen_func_in_params(signal["params"]);
+    out << ")";
+    gen_func_in_return(signal["return"]);
+    out << ";" << std::endl;
+}
+
+void NSLGen::gen_func_in_params(const std::vector<json> &params) {
+    if(params.size() > 0)
+    {
+        for(auto &param : params)
+        {
+            out << param.get<std::string>();
+            if(param != params.back())
+            {
+                out << ", ";
+            }
+        }
+    }
+}
+
+void NSLGen::gen_func_in_return(const json &ret) {
+    if(!ret.is_null())
+    {
+        out << " : ";
+        out << ret.get<std::string>();
+    }
+}
+
+void NSLGen::gen_func_out(json &signal) {
+    out << "func_out " << signal["name"].get<std::string>() << "(";
+    gen_func_out_params(signal["params"]);
+    out << ")";
+    gen_func_out_return(signal["return"]);
+    out << ";" << std::endl;
+}
+
+void NSLGen::gen_func_out_params(const std::vector<json> &params) {
+    if(params.size() > 0)
+    {
+        for(auto &param : params)
+        {
+            out << param.get<std::string>();
+            if(param != params.back())
+            {
+                out << ", ";
+            }
+        }
+    }
+}
+
+void NSLGen::gen_func_out_return(const json &ret) {
+    if(!ret.is_null())
+    {
+        out << " : ";
+        out << ret.get<std::string>();
+    }
+}
+
 void NSLGen::gen(std::map<std::string, json> &modules)
 {
-    out << "NSLGen::gen" << std::endl;
     for(auto &module : modules)
     {
-        out << "module: " << module.first << std::endl;
-        out << "module: " << module.second.dump(4) << std::endl;
+        gen_declare(module.first, module.second["signals"]);
+    
     }
+}
+
+void NSLGen::gen_opening_brace()
+{
+    out << " {" << std::endl;
+    indent++;
+    out.width(indent * 4);
+}
+
+void NSLGen::gen_closing_brace()
+{
+    out << "}" << std::endl;
+    indent--;
+    out.width(indent * 4);
 }
--- nslgen.hh
+++ nslgen.hh
@@ -10,7 +10,19 @@ public:
     void gen(std::map<std::string, json> &modules);
 
 private:
+    void gen_declare(const std::string &name, json &signals);
+    void gen_input(json &signal);
+    void gen_output(json &signal);
+    void gen_func_in(json &signal);
+    void gen_func_out(json &signal);
+    void gen_func_in_params(const std::vector<json> &params);
+    void gen_func_in_return(const json &ret);
+    void gen_func_out_params(const std::vector<json> &params);
+    void gen_func_out_return(const json &ret);
+    void gen_opening_brace();
+    void gen_closing_brace();
     std::ostream &out;
+    size_t indent = 0;
 };
 
 #endif
