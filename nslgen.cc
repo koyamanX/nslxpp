abca9efac120fcd03c9c0db1422778fc14bacab6
koyamanX <ckoyama.1996@gmail.com>
2024-01-08 04:25:21 +0900

    WIP

--- nslgen.cc
+++ nslgen.cc
@@ -17,19 +17,22 @@ void NSLGen::gen_declare(const std::string &name, const std::map<std::string, js
     for(auto &i : symtab)
     {
         auto &signal = i.second;
-        switch(signal["type"].get<NSLXPP::NodeType>())
+        auto signal_type = signal["type"].get<NSLXPP::NodeType>();
+        auto signal_node = signal.get<json>();
+
+        switch(signal_type)
         {
             case NSLXPP::ND_INPUT:
-                gen_input(signal.get<json>());
+                gen_input(signal_node);
                 break;
             case NSLXPP::ND_OUTPUT:
-                gen_output(signal.get<json>());
+                gen_output(signal_node);
                 break;
             case NSLXPP::ND_FUNC_IN:
-                gen_func_in(signal.get<json>());
+                gen_func_in(signal_node);
                 break;
             case NSLXPP::ND_FUNC_OUT:
-                gen_func_out(signal.get<json>());
+                gen_func_out(signal_node);
                 break;
         }
     }
@@ -113,7 +116,6 @@ void NSLGen::gen(std::map<std::string, json> &modules)
         auto &name = module.first;
         auto symtab = module.second["signals"].get<std::map<std::string, json>>();
         gen_declare(name, symtab);
-    
     }
 }
 
