28a6877fb232c86cfce945c4a003c7ba65531b22
koyamanX <ckoyama.1996@gmail.com>
2024-01-08 19:58:01 +0900

    WIP

--- nslgen.cc
+++ nslgen.cc
@@ -23,10 +23,10 @@ void NSLGen::gen_declare(const std::string &name,
         gen_output(signal_node);
         break;
       case NSLXPP::ND_FUNC_IN:
-        gen_func_in(signal_node);
+        gen_func_in(signal_node, symtab);
         break;
       case NSLXPP::ND_FUNC_OUT:
-        gen_func_out(signal_node);
+        gen_func_out(signal_node, symtab);
         break;
     }
   }
@@ -47,28 +47,40 @@ void NSLGen::gen_output(const json &signal) {
   out << "output " << signal_name << "[" << signal_size << "];" << std::endl;
 }
 
-void NSLGen::gen_func_in(const json &signal) {
+void NSLGen::gen_func_in(const json &signal, const std::map<std::string, json> &symtab) {
   auto signal_name = signal["name"].get<std::string>();
 
   out << "func_in " << signal_name << "(";
   if (signal.contains("params")) {
     auto params_node = signal["params"].get<std::vector<json>>();
 
-    gen_func_in_params(params_node);
+    gen_func_in_params(params_node, symtab);
   }
   out << ")";
   if (signal.contains("return")) {
     auto return_node = signal["return"].get<json>();
 
-    gen_func_in_return(return_node);
+    gen_func_in_return(return_node, symtab);
   }
   out << ";" << std::endl;
 }
 
-void NSLGen::gen_func_in_params(const std::vector<json> &params) {
+void NSLGen::gen_func_in_params(const std::vector<json> &params, const std::map<std::string, json> &symtab) {
   if (params.size() > 0) {
     for (auto &param : params) {
       auto param_name = param.get<std::string>();
+      auto symbol = symtab.find(param_name);
+
+      if(symbol == symtab.end()) {
+        out << "ERROR: " << param_name << " is not a signal" << std::endl;
+        exit(1);
+      }
+      auto symbol_type = symbol->second["type"].get<NSLXPP::NodeType>();
+
+      if(symbol_type != NSLXPP::ND_INPUT) {
+        out << "ERROR: " << param_name << " is not an input signal" << std::endl;
+        exit(1);
+      }
 
       out << param_name;
       if (param != params.back()) {
@@ -78,35 +90,59 @@ void NSLGen::gen_func_in_params(const std::vector<json> &params) {
   }
 }
 
-void NSLGen::gen_func_in_return(const json &ret) {
+void NSLGen::gen_func_in_return(const json &ret, const std::map<std::string, json> &symtab) {
   auto return_name = ret.get<std::string>();
+  auto symbol = symtab.find(return_name);
+  
+  if(symbol == symtab.end()) {
+    out << "ERROR: " << return_name << " is not a signal" << std::endl;
+    exit(1);
+  }
+  auto symbol_type = symbol->second["type"].get<NSLXPP::NodeType>();
+
+  if(symbol_type != NSLXPP::ND_OUTPUT) {
+    out << "ERROR: " << return_name << " is not an output signal" << std::endl;
+    exit(1);
+  }
 
   out << " : ";
   out << return_name;
 }
 
-void NSLGen::gen_func_out(const json &signal) {
+void NSLGen::gen_func_out(const json &signal, const std::map<std::string, json> &symtab) {
   auto signal_name = signal["name"].get<std::string>();
 
   out << "func_out " << signal_name << "(";
   if (signal.contains("params")) {
     auto params_node = signal["params"].get<std::vector<json>>();
 
-    gen_func_out_params(params_node);
+    gen_func_out_params(params_node, symtab);
   }
   out << ")";
   if (signal.contains("return")) {
     auto return_node = signal["return"].get<json>();
 
-    gen_func_out_return(return_node);
+    gen_func_out_return(return_node, symtab);
   }
   out << ";" << std::endl;
 }
 
-void NSLGen::gen_func_out_params(const std::vector<json> &params) {
+void NSLGen::gen_func_out_params(const std::vector<json> &params, const std::map<std::string, json> &symtab) {
   if (params.size() > 0) {
     for (auto &param : params) {
       auto param_name = param.get<std::string>();
+      auto symbol = symtab.find(param_name);
+
+      if(symbol == symtab.end()) {
+        out << "ERROR: " << param_name << " is not a signal" << std::endl;
+        exit(1);
+      }
+      auto symbol_type = symbol->second["type"].get<NSLXPP::NodeType>();
+
+      if(symbol_type != NSLXPP::ND_OUTPUT) {
+        out << "ERROR: " << param_name << " is not an output signal" << std::endl;
+        exit(1);
+      }
 
       out << param_name;
       if (param != params.back()) {
@@ -116,8 +152,20 @@ void NSLGen::gen_func_out_params(const std::vector<json> &params) {
   }
 }
 
-void NSLGen::gen_func_out_return(const json &ret) {
+void NSLGen::gen_func_out_return(const json &ret, const std::map<std::string, json> &symtab) {
   auto return_name = ret.get<std::string>();
+  auto symbol = symtab.find(return_name);
+
+  if(symbol == symtab.end()) {
+    out << "ERROR: " << return_name << " is not a signal" << std::endl;
+    exit(1);
+  }
+  auto symbol_type = symbol->second["type"].get<NSLXPP::NodeType>();
+
+  if(symbol_type != NSLXPP::ND_INPUT) {
+    out << "ERROR: " << return_name << " is not an input signal" << std::endl;
+    exit(1);
+  }
 
   out << " : ";
   out << return_name;
